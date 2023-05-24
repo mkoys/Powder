16dedb3453a3584e1ae31ee70965c82348492fef
mkoys <mkoys@protonmail.com>
2023-05-24 13:15:01 +0200

    File handler
    HTTP server now has File Handler that resides in request listener. The
    File Handler respondes with content of file based on url provided in
    HTTP request.

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 import http from "http";
+import fs from "fs";
+import path from "path";
 
 const port = 3000;
 
@@ -7,5 +9,16 @@ const server = http.createServer(requestListener);
 server.listen(port, () => console.log(`Running on http://127.0.0.1:${port}`));
 
 function requestListener(request, response) {
-	response.end("Hello World!");
+	const splitUrl = request.url.split("/");
+	const filterRule = item => item.length > 0 && item !== "..";
+	const filteredUrl = splitUrl.filter(filterRule);
+	const fileUrl = filteredUrl.join("/");
+	
+	const currentPath = new URL("..", import.meta.url).href;
+	const filePath = path.join(currentPath, fileUrl);
+	
+	fs.readFile(filePath, (error, file) => {
+		if(error) response.statusCode = 404;
+		response.end(file);
+	});
 }
